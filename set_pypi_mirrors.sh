d0770e7448d29b22216a6a948415170971676a54
chinaheyu <810130242@qq.com>
2020-08-21 04:20:59 -0700

    发布1.0

--- set_pypi_mirrors.sh
+++ set_pypi_mirrors.sh
@@ -1,3 +1,7 @@
-sudo apt -y install python3-pip
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
-pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+echo "install python3-pip ..."
+sudo apt -y install python3-pip > /dev/null 2>&1
+echo "update pip3 ..."
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U > /dev/null 2>&1
+echo "set mirrors ..."
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null 2>&1
+echo "finished"
